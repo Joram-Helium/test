a8857609a85faefc3d40bc407d381d2e0404d4b4
Joram-Helium <107384074+Joram-Helium@users.noreply.github.com>
2024-01-30 17:06:37 +1100

    Create update-pr-base.js

---
+++ update-pr-base.js
@@ -0,0 +1,35 @@
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+async function updatePRBase(repositoryOwner, repositoryName, prNumber, newBase) {
+  await octokit.rest.pulls.update({
+    owner: repositoryOwner,
+    repo: repositoryName,
+    pull_number: prNumber,
+    base: newBase
+  });
+}
+
+async function run() {
+  const repositoryOwner = process.env.GITHUB_REPOSITORY.split('/')[0];
+  const repositoryName = process.env.GITHUB_REPOSITORY.split('/')[1];
+  const mergedBranch = process.env.GITHUB_HEAD_REF;
+
+  const { data: pullRequests } = await octokit.rest.pulls.list({
+    owner: repositoryOwner,
+    repo: repositoryName,
+    state: 'open'
+  });
+
+  for (const pr of pullRequests) {
+    if (pr.base.ref === mergedBranch) {
+      await updatePRBase(repositoryOwner, repositoryName, pr.number, process.env.DEFAULT_BRANCH);
+      console.log(`Updated base of PR #${pr.number} to ${process.env.DEFAULT_BRANCH}`);
+    }
+  }
+}
+
+run().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
